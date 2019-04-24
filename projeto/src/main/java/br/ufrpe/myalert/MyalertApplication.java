4aca5273dee61294aaf701c7ad5c38d2ba0added
Maik Luiz Paixão <paixomaik@outlook.com.br>
2019-04-24 19:55:55 -0300

    Adicionei estrutura do projeto

---
+++ projeto/src/main/java/br/ufrpe/myalert/MyalertApplication.java
@@ -0,0 +1,13 @@
+package br.ufrpe.myalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyalertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyalertApplication.class, args);
+    }
+
+}
