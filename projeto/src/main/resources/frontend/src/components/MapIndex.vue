e46da69a632d0bb58137a9163f205a02f9866a1e
Maik Luiz Paixão <paixomaik@outlook.com.br>
2019-05-03 20:28:34 -0300

    login page added

---
+++ projeto/src/main/resources/frontend/src/components/MapIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+ 
+<script>
+  export default {
+    name: "map-index"
+  };
+</script>
+ 
+<style>
+.submitform {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
--- projeto/target/classes/frontend/src/App.vue
+++ projeto/target/classes/frontend/src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
           <div class="container">
-            <a class="navbar-brand" href="/">
+            <a class="navbar-brand" href="#">
               <img class="logo-brand" src="./assets/logo_mylaert.png"/>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
@@ -11,7 +11,7 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
               <ul class="navbar-nav">
                 <li class="nav-item active">
-                  <a class="nav-link" href="/">Página Inicial <span class="sr-only">(Página atual)</span></a>
+                  <a class="nav-link" href="#">Página Inicial <span class="sr-only">(Página atual)</span></a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="#">Sobre Nós</a>
@@ -22,19 +22,19 @@
                 <li class="nav-item">
                   <a class="nav-link" href="#">Contato</a>
                 </li>
-                <li class="active nav-item">
-                  <a href="/login" class="nav-link btn btn-danger px-3">Login</a>
+                <li class="nav-item">
+                  <a href="#" class="nav-link btn btn-dark px-3">Login</a>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
         <br/>
-
         <router-view/>
-        <map-index></map-index>
-      </div>
-      
+        <figure>
+          <img src="https://olhardigital.com.br/uploads/acervo_imagens/2019/03/r16x9/20190301101226_1200_675.jpg"/>
+        </figure>
+    </div>
 </template>
 
 <script>
