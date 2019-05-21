f8143918f1e6460d974c724f41f866977678d540
Gabriel Albuquerque de Oliveira <gabriel.oliveira.gt@gmail.com>
2019-05-21 01:25:58 -0300

    Chamando o componente sem precisar apelidar

--- projeto/src/main/resources/frontend/src/components/Index.vue
+++ projeto/src/main/resources/frontend/src/components/Index.vue
@@ -1,11 +1,11 @@
 
 <template>
         <div id="index">
-          <map-index></map-index>
-          <about-partial></about-partial>
-          <video-partial></video-partial>
-          <info-partial></info-partial>
-          <callback-partial></callback-partial>
+            <MapIndex/>
+            <AboutPartial/>
+            <VideoPartial/>
+            <InfoPartial/>
+            <CallbackPartial/>
         </div>
 </template>
  
@@ -18,11 +18,11 @@ import CallbackPartial from "./index/callback_partial.vue";
 
 export default {
   components: {
-        'map-index': MapIndex,
-        'about-partial': AboutPartial,
-        'video-partial': VideoPartial,
-        'info-partial': InfoPartial,
-        'callback-partial': CallbackPartial
+        MapIndex,
+        AboutPartial,
+        VideoPartial,
+        InfoPartial,
+        CallbackPartial
       },      
   name: "index"
 };
