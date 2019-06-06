98e378496f600f7541b07cde4e3b31e499196ba0
Elthon Ferreira <elthonferreira18@gmail.com>
2019-06-06 18:09:10 -0300

    modiquei o front

--- projeto/src/main/resources/frontend/src/http-common.js
+++ projeto/src/main/resources/frontend/src/http-common.js
@@ -1,6 +1,7 @@
 import axios from "axios";
  
 export default axios.create({
+ // baseURL: "http://eb333f79.ngrok.io",//,//api",
   baseURL: "http://localhost:8080",//,//api",
   headers: {
     "Content-type": "application/json",
