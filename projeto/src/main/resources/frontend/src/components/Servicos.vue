8424cbc9ce6be04f554ac7e5f3c9e9f6f416bf1d
Maik Luiz Paixão <paixomaik@outlook.com.br>
2019-06-08 10:08:54 -0300

    refatoração páginas serviços e dúvidas

--- projeto/src/main/resources/frontend/src/components/Servicos.vue
+++ projeto/src/main/resources/frontend/src/components/Servicos.vue
@@ -3,7 +3,14 @@
 
  <div class="service=wrapper">
     <Navbar/>
-    <PageHeader/>
+    <PageHeader :duvProps="pageHeader"></PageHeader>
+    <div class="container mt-5 mb-5">
+                        <div class="card-deck">
+                              <CardServices/>
+                              <CardServices/>
+                              <CardServices/>
+                        </div>
+                </div>
   </div>
 </template>
 
@@ -11,20 +18,22 @@
   import { mapMutations } from 'vuex';
   import Navbar from "./Navbar.vue";
   import PageHeader from "./PageHeader.vue";
+  import CardServices from "./cards/CardServices.vue";
 
 export default {
   components: {
     Navbar,
-    PageHeader
+    PageHeader,
+    CardServices
   },
       name: "servicos",
-      methods: {
-        ...mapMutations([
-          'NAV_ATIVO'
-        ])
-      },
-      created(){
-        this.NAV_ATIVO('servicos')
+      data(){
+        return{
+          pageHeader: {
+            title: 'Serviços',
+            description: 'A nossa plataforma fornece diversos serviços que visam ajudar as pessoas que estejam em áreas de risco.'
+          }
+      }
       }
     };    
 </script>
---
+++ projeto/src/main/resources/frontend/src/components/cards/CardServices.vue
@@ -0,0 +1,20 @@
+ 
+ <template>
+        <div class="card">
+                <img class="card-img-top" src="../../assets/enchentes.png" alt="Card image cap">
+                
+                <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+
+         </div>
+</template>
+
+<script>
+
+export default {
+      name: "card_services"
+    };    
+</script>
