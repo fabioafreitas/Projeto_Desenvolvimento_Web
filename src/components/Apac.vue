915bf430c069d2f6c87e51eb7e3694bab76263c9
fabio <fbio.alves095@gmail.com>
2019-06-22 00:56:19 -0300

    ...

---
+++ src/components/Apac.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="container-fluid lay-site-shadow" style="height:700px">
+		<div class="lay-site">
+			<header class="lay-header ">
+					<div class="row justify-content-center">
+						<div class="col text-center">
+							<a class="navbar-brand" href="/">
+								<img class="img-fluid" src="imagens/logo.png" alt="logo"
+										style="max-width:200%; padding: 10%">
+							</a>
+							<a class="navbar-brand" href="#">
+								<img class="img-fluid" src="imagens/apac-logo.png" alt="logo"
+										style="max-width:80%; padding: 10%">
+							</a>
+						</div>
+					</div>
+			</header>
+			<table class="table table-hover">
+				<thead class="thead-info">
+					<tr>
+						<th>TÍTULO</th>
+						<th>CPF</th>
+						<th>DESCRIÇÃO</th>
+						<th>URGÊNCIA</th>
+						<th>CATEGORIA</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="ocorrencia of ocorrencias" :key="ocorrencia.key">
+						<td>{{ ocorrencia.titulo }}</td>
+						<td>{{ ocorrencia.cpf }}</td>
+						<td>{{ ocorrencia.descricao }}</td>
+						<td>{{ ocorrencia.urgencia }}</td>
+						<td>{{ ocorrencia.categoria }}</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+name: "apac",
+data() {
+    return {
+    ocorrencias: []
+    };
+},
+methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Samu.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+				<div class="container lay-site-shadow" style="height:700px">
+						<div class="lay-site">
+								<header class="lay-header ">
+										<div class="row justify-content-center">
+												<div class="col text-center">
+														<a class="navbar-brand" href="/">
+																<img class="img-fluid" src="imagens/logo.png" alt="logo"
+																		style="max-width:200%; padding: 10%">
+														</a>
+														<a class="navbar-brand" href="#">
+																<img class="img-fluid" src="imagens/samu-logo.png" alt="logo"
+																		style="max-width:60%; padding: 10%">
+														</a>
+												</div>
+										</div>
+								</header>
+								<table class="table table-hover">
+												<thead class="thead-info">
+														<tr>
+																<th>TÍTULO</th>
+																<th>CPF</th>
+																<th>DESCRIÇÃO</th>
+																<th>URGÊNCIA</th>
+																<th>CATEGORIA</th>
+														</tr>
+												</thead>
+												<tbody>
+														<tr v-for="ocorrencia of ocorrencias" :key="ocorrencia.key">
+																<td>{{ ocorrencia.titulo }}</td>
+																<td>{{ ocorrencia.cpf }}</td>
+																<td>{{ ocorrencia.descricao }}</td>
+																<td>{{ ocorrencia.urgencia }}</td>
+																<td>{{ ocorrencia.categoria }}</td>
+														</tr>
+												</tbody>
+										</table>
+								<div class="lay-main-bg">
+										
+								</div>
+						</div>
+				</div>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "samu",
+  data() {
+    return {
+      ocorrencias: []
+    };
+  },
+  methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
