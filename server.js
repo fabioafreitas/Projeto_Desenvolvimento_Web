c7b0f1d47756853523bf239aa05e59ff64c23619
fabio <fbio.alves095@gmail.com>
2019-06-22 16:46:56 -0300

    ...

---
+++ server.js
@@ -0,0 +1,19 @@
+const history = require('connect-history-api-fallback')
+const express = require('express')
+const path    = require('path')
+
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname, 'dist'))
+
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/index.html'))
+})
+
+var port = process.env.PORT || 4250
+app.listen(port, function () {
+    console.log( 'Express serving on 4250!' )
+})
