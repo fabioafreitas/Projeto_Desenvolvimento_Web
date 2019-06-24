cc2fee37caec988e13ea03117b3d1699573629b9
fabio <fbio.alves095@gmail.com>
2019-06-24 20:35:51 -0300

    ...

--- projeto/src/main/resources/frontend/src/http-common.js
+++ projeto/src/main/resources/frontend/src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://my-alert-backend.herokuapp.com"    /* Antes do deploy, lembrar de utilizar esta URL */
-  baseURL: "http://localhost:8080",
+  baseURL: "https://my-alert-backend.herokuapp.com",    /* Antes do deploy, lembrar de utilizar esta URL */
+  //baseURL: "http://localhost:8080",
   headers: {
     "Content-type": "application/json",
   }
